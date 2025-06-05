5283e02143b0b5caebb7674d3707c43b7e7f1ae9
Dhruv Patel <dhruv.nanosoft@gmail.com>
2025-06-05 09:02:34 +0100

    refactor

--- editor.mjs
+++ editor.mjs
@@ -1,11 +1,11 @@
-import {basicSetup} from "codemirror"
-import {indentWithTab,defaultKeymap} from "@codemirror/commands"
-import {EditorState, StateEffect, StateField} from "@codemirror/state"
-import {EditorView, keymap, Decoration} from "@codemirror/view"
+import { basicSetup } from "codemirror"
+import { indentWithTab, defaultKeymap } from "@codemirror/commands"
+import { EditorState, StateEffect, StateField } from "@codemirror/state"
+import { EditorView, keymap, Decoration } from "@codemirror/view"
 
 import MMIXAL from "./dist/mmixal.js"
 import MMIX from "./dist/mmix.js"
-import {parse_mmo} from "./mmix_object_reader.mjs"
+import { parse_mmo } from "./mmix_object_reader.mjs"
 
 
 
@@ -22,42 +22,32 @@ Main	LDA		$255,Text
 `
 
 const setHighlightLine = StateEffect.define();
-const removeAnyHighlight = StateEffect.define();
 const lineHighlightDecoration = Decoration.line({
-    attributes: {style: 'background-color: yellow'}
+    attributes: { style: 'background-color: yellow' }
 });
 const lineHighlightField = StateField.define({
     create() {
-      return Decoration.none;
+        return Decoration.none;
     },
     update(lines, tr) {
-      lines = lines.map(tr.changes);
-      for (let e of tr.effects) if (e.is(setHighlightLine)) {
-        lines = Decoration.set([lineHighlightDecoration.range(e.value)]);
-      }
-      for (let e of tr.effects) if (e.is(removeAnyHighlight)) {
-        lines = Decoration.none;
-      }
-      return lines;
+        lines = lines.map(tr.changes);
+        for (let e of tr.effects) if (e.is(setHighlightLine)) {
+            if (e.value === null) {
+                lines = Decoration.none;
+            } else {
+                lines = Decoration.set([lineHighlightDecoration.range(e.value)]);
+            }
+        }
+        return lines;
     },
     provide: (f) => EditorView.decorations.from(f),
-  });
-
-let state = EditorState.create({
-  doc: hello_world_src,
-  extensions: [basicSetup, keymap.of([indentWithTab, defaultKeymap]), lineHighlightField],
-})
+});
 
 let editor = new EditorView({
-  state: state,
-  parent: document.getElementById("editor-div"),
+    doc: hello_world_src,
+    parent: document.getElementById("editor-div"),
+    extensions: [basicSetup, keymap.of([indentWithTab, defaultKeymap]), lineHighlightField],
 })
-
-
-let all_regs = [];
-for (let i = 0; i <= 255; i++) all_regs.push("r" + i);
-for (let i = 65; i <= 90; i++) all_regs.push("r" + String.fromCharCode(i));
-for (let i of ['BB', 'TT', 'WW', 'XX', 'YY', 'ZZ']) all_regs.push("r" + i);
 
 
 let mmix_state = {
@@ -67,12 +57,9 @@ let mmix_state = {
     next_command: undefined,
     capture_stdout: true,
     stdout_buffer: "",
-    common_registers: [],
-    special_registers: [],
-    format: "!",
     ready_for_next_action: false,
 
-    init: async function(mmo_content) {
+    init: async function (mmo_content) {
         let MMIX_Module = {
             preRun: () => {
                 mmix_state.stderr_buffer = [];
@@ -82,17 +69,17 @@ let mmix_state = {
         }
 
         const mmix = await MMIX(MMIX_Module);
+        this.mmix = mmix;
         this.loc_to_line = parse_mmo(mmo_content);
         this.next_command = undefined;
         this.capture_stdout = true;
         this.stdout_buffer = "";
         this.ready_for_next_action = true;
-        this.mmix = mmix;
-        mmix.FS.writeFile("./code.mmo", mmo_content, {encoding: "binary"});
+        mmix.FS.writeFile("./code.mmo", mmo_content, { encoding: "binary" });
         mmix.callMain(["-i", "./code.mmo"]);
     },
 
-    send_command: function(cmd) {
+    send_command: function (cmd) {
         this.ready_for_next_action = false;
         this.next_command(cmd);
         this.next_command = () => {
@@ -100,7 +87,7 @@ let mmix_state = {
         };
     },
 
-    pause_until_ready: async function() {
+    pause_until_ready: async function () {
         return new Promise((resolve) => {
             const poll = () => {
                 if (this.ready_for_next_action) {
@@ -113,13 +100,13 @@ let mmix_state = {
         });
     },
 
-    send_command_with_output: async function(cmd) {
+    send_command_with_output: async function (cmd) {
         await this.pause_until_ready();
         const originalBuffer = this.stdout_buffer;
         this.stdout_buffer = "";
         this.capture_stdout = false;
         this.send_command(cmd);
-       
+
         const output = await new Promise((resolve) => {
             const poll = () => {
                 if (this.stdout_buffer.endsWith("mmix> ")) {
@@ -136,48 +123,56 @@ let mmix_state = {
         return output;
     },
 
-    step: async function() {
+    step: async function () {
         await this.pause_until_ready();
         this.send_command("\n");
         await this.maybe_set_hightlight();
     },
 
 
-    get_current_location: async function() {
+    get_current_location: async function () {
         await this.pause_until_ready();
         const output = await this.send_command_with_output("s\n");
         const match = output.match(/now at location #([0-9A-Fa-f]+)/);
 
         if (match) {
-          const addressBigInt = BigInt('0x' + match[1]);
-          return this.loc_to_line[addressBigInt];
+            const addressBigInt = BigInt('0x' + match[1]);
+            return this.loc_to_line[addressBigInt];
         } else {
-          console.log("No address found in the line.");
-          return undefined;
+            console.log("No address found in the line.");
+            return undefined;
         }
     },
 
-    maybe_set_hightlight: async function() {
+    maybe_set_hightlight: async function () {
         const line = await this.get_current_location();
         if (line !== undefined) {
             highlightLine(line);
             const pos = editor.state.doc.line(line).from;
-            editor.dispatch({
-                effects: EditorView.scrollIntoView(pos, {
-                  y: "center",
-                }),
-              });
+            var should_scroll = true;
+            for (const range of editor.visibleRanges) {
+                if (pos >= range.from && pos <= range.to) {
+                    should_scroll = false;
+                }
+            }
+            if (should_scroll) {
+                editor.dispatch({
+                    effects: EditorView.scrollIntoView(pos, {
+                        y: "center",
+                    }),
+                });
+            }
         } else {
             removeHighlight();
         }
     },
 
-    get_register: async function(register, format="!") {
+    get_register: async function (register, format = "!") {
         const output = await this.send_command_with_output(`${register}${format}\n`);
         return output.split('=').pop().split('\n')[0];
     },
 
-    update_register_table: async function(format="!", change_visibility=true) {
+    update_register_table: async function (format = "!", change_visibility = true) {
         console.log("Updating registers with format:", format);
         const rL = parseInt(await this.get_register("rL", "!"));
         const rG = parseInt(await this.get_register("rG", "!"));
@@ -202,16 +197,16 @@ async function compile() {
     let txt = editor.state.doc.toString();
     let maxLength = 80;
     for (let i = 0; i < editor.state.doc.lines; i++) {
-      const line = editor.state.doc.line(i + 1);
-      if (line.length > maxLength) {
-        maxLength = line.length + 1;;
-      }
+        const line = editor.state.doc.line(i + 1);
+        if (line.length > maxLength) {
+            maxLength = line.length + 1;;
+        }
     }
     const alfs = mmixal.FS;
-    alfs.writeFile("./code.mms", txt, {encoding: "ascii"});
+    alfs.writeFile("./code.mms", txt, { encoding: "ascii" });
     mmixal.callMain([`-b ${maxLength}`, "./code.mms"]);
 
-    let content = alfs.readFile("./code.mmo", {encoding: "binary"});
+    let content = alfs.readFile("./code.mmo", { encoding: "binary" });
 
     return content;
 }
@@ -225,7 +220,7 @@ function stdout_fn(text) {
         stdout_area.innerHTML += mmix_state.stdout_buffer;
         mmix_state.stdout_buffer = "";
         stdout_area.scrollTop = stdout_area.scrollHeight;
-    } 
+    }
 }
 
 async function compileStep() {
@@ -257,18 +252,18 @@ step_btn.addEventListener("click", async (e) => {
 
 
 function submit_command() {
-    return new Promise((accept, reject)=>{
+    return new Promise((accept, reject) => {
         mmix_state.next_command = accept;
     });
 }
 
 function highlightLine(lineNo) {
     const docPosition = editor.state.doc.line(lineNo).from;
-    editor.dispatch({effects: setHighlightLine.of(docPosition)});
+    editor.dispatch({ effects: setHighlightLine.of(docPosition) });
 }
 
 function removeHighlight() {
-    editor.dispatch({effects: removeAnyHighlight.of()});
+    editor.dispatch({ effects: setHighlightLine.of(null) });
 }
 
 globalThis.editor = editor;
@@ -288,7 +283,7 @@ github_load_btn.addEventListener("click", async (e) => {
     github_url = github_url.replace("//github.com", "//raw.githubusercontent.com");
     github_url = github_url.replace("blob/", "/");
 
-    
+
     const error_box = document.getElementById("github-error-box");
 
 
@@ -300,7 +295,7 @@ github_load_btn.addEventListener("click", async (e) => {
     const dialog = document.getElementById("load-github-dialog");
     if (response.ok) {
         const text = await response.text();
-        editor.dispatch({changes: {from: 0, to: editor.state.doc.length, insert: text}});
+        editor.dispatch({ changes: { from: 0, to: editor.state.doc.length, insert: text } });
         error_box.innerHTML = "";
         compileStep();
         dialog.close();
